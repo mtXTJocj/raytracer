9fef40935bcc37118ae530ad1861e94eb1b677f9
mtXTJocj <mtXTJocj@yahoo.co.jp>
2019-08-12 18:17:52 +0900

    Point3D creation.

--- src/point3d.rs
+++ src/point3d.rs
@@ -5,3 +5,28 @@ pub struct Point3D {
     pub y: f32,
     pub z: f32,
 }
+
+impl Point3D {
+    /// 新しい Point3D を作成する
+    ///
+    /// # Argumets
+    /// * `x` - x
+    /// * `y` - y
+    /// * `z` - z
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Point3D { x, y, z }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn point_creation() {
+        let point = Point3D::new(4.3, -4.2, 3.1);
+        assert_eq!(4.3, point.x);
+        assert_eq!(-4.2, point.y);
+        assert_eq!(3.1, point.z);
+    }
+}
