f9bef1cde4731157f0595338880e810bd88f0fea
mtXTJocj <mtXTJocj@yahoo.co.jp>
2019-11-03 11:42:52 +0900

    added color constants(BLACK and WHITE).

--- src/color.rs
+++ src/color.rs
@@ -11,6 +11,17 @@ pub struct Color {
 }
 
 impl Color {
+    pub const BLACK: Color = Color {
+        red: 0.0,
+        green: 0.0,
+        blue: 0.0,
+    };
+    pub const WHITE: Color = Color {
+        red: 1.0,
+        green: 1.0,
+        blue: 1.0,
+    };
+
     /// 新しい Color を作成する
     ///
     /// # Argumets
