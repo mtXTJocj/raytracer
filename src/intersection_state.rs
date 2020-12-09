24033867be33ad6de2d311001a830c62f51c5c4f
mtXTJocj <mtXTJocj@yahoo.co.jp>
2020-12-09 11:51:39 +0900

    IntersectionState の初期化処理

--- src/intersection_state.rs
+++ src/intersection_state.rs
@@ -1,4 +1,7 @@
-use super::{point3d::Point3D, sphere::Sphere, vector3d::Vector3D};
+use super::{
+    intersection::Intersection, point3d::Point3D, ray::Ray, sphere::Sphere,
+    vector3d::Vector3D,
+};
 
 #[derive(Debug)]
 pub(crate) struct IntersectionState<'a> {
@@ -7,4 +10,53 @@ pub(crate) struct IntersectionState<'a> {
     pub(crate) point: Point3D,
     pub(crate) eyev: Vector3D,
     pub(crate) normalv: Vector3D,
+    pub(crate) inside: bool,
+}
+
+impl<'a> IntersectionState<'a> {
+    pub(crate) fn new(i: &'a Intersection, r: &Ray) -> Self {
+        let t = i.t;
+        let object = i.object;
+        let point = r.position(i.t);
+        let eyev = -r.direction();
+        let normalv = object.normal_at(&point);
+        let inside = if normalv.dot(&eyev) < 0.0 {
+            true
+        } else {
+            false
+        };
+
+        IntersectionState {
+            t,
+            object,
+            point,
+            eyev,
+            normalv,
+            inside,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn precomputing_the_state_of_intersection() {
+        let r = Ray::new(
+            Point3D::new(0.0, 0.0, -5.0),
+            Vector3D::new(0.0, 0.0, 1.0),
+        );
+        let shape = Sphere::new();
+        let i = Intersection {
+            t: 4.0,
+            object: &shape,
+        };
+
+        let comps = IntersectionState::new(&i, &r);
+        assert_eq!(i.t, comps.t);
+        assert_eq!(Point3D::new(0.0, 0.0, -1.0), comps.point);
+        assert_eq!(Vector3D::new(0.0, 0.0, -1.0), comps.eyev);
+        assert_eq!(Vector3D::new(0.0, 0.0, -1.0), comps.normalv);
+    }
 }
