ba217bf2aed2cc1dd11473c6d42b46497cf9672b
mtXTJocj <mtXTJocj@yahoo.co.jp>
2020-12-15 15:51:26 +0900

    pixel_size 計算

--- src/camera.rs
+++ src/camera.rs
@@ -6,22 +6,41 @@ pub struct Camera {
     vsize: usize,
     field_of_view: f32,
     transform: Transform,
+    half_width: f32,
+    half_height: f32,
+    pixel_size: f32,
 }
 
 impl Camera {
     pub fn new(hsize: usize, vsize: usize, field_of_view: f32) -> Self {
+        let half_view = (field_of_view / 2.0).tan();
+        let aspect = hsize as f32 / (vsize as f32);
+        let half_width;
+        let half_height;
+        if aspect >= 1.0 {
+            half_width = half_view;
+            half_height = half_view / aspect;
+        } else {
+            half_width = half_view * aspect;
+            half_height = half_view;
+        }
+        let pixel_size = (half_width * 2.0) / (hsize as f32);
+
         Camera {
             hsize,
             vsize,
             field_of_view,
             transform: Transform::identity(),
+            half_width,
+            half_height,
+            pixel_size,
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::{super::vector3d::Vector3D, *};
 
     #[test]
     fn constructing_camera() {
@@ -34,5 +53,17 @@ mod tests {
         assert_eq!(120, c.vsize);
         assert_eq!(std::f32::consts::FRAC_PI_2, c.field_of_view);
         assert_eq!(Transform::identity(), c.transform);
+    }
+
+    #[test]
+    fn the_pixel_size_for_a_horizontal_canvas() {
+        let c = Camera::new(200, 125, std::f32::consts::FRAC_PI_2);
+        assert_eq!(0.01, c.pixel_size);
+    }
+
+    #[test]
+    fn the_pixel_size_for_a_vertical_canvas() {
+        let c = Camera::new(125, 200, std::f32::consts::FRAC_PI_2);
+        assert_eq!(0.01, c.pixel_size);
     }
 }
