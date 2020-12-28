0df54b6f9982f349cccb7701e5aa8b57d18f74ff
mtXTJocj <mtXTJocj@yahoo.co.jp>
2020-12-28 13:15:54 +0900

    add tests.

--- src/pattern.rs
+++ src/pattern.rs
@@ -14,3 +14,71 @@ pub trait Pattern: Debug {
         self.pattern_at(&pattern_p)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{super::sphere::Sphere, *};
+
+    #[derive(Debug)]
+    struct TestPattern {
+        transform: Transform,
+    }
+
+    impl TestPattern {
+        fn new() -> Self {
+            TestPattern {
+                transform: Transform::identity(),
+            }
+        }
+    }
+
+    impl Pattern for TestPattern {
+        fn transform(&self) -> &Transform {
+            &self.transform
+        }
+
+        fn transform_mut(&mut self) -> &mut Transform {
+            &mut self.transform
+        }
+
+        fn pattern_at(&self, p: &Point3D) -> Color {
+            Color::new(p.x, p.y, p.z)
+        }
+    }
+
+    #[test]
+    fn the_default_pattern_transformation() {
+        let pattern = TestPattern::new();
+
+        assert_eq!(&Transform::identity(), pattern.transform());
+    }
+
+    #[test]
+    fn assigning_a_transformation() {
+        let mut pattern = TestPattern::new();
+        *pattern.transform_mut() = Transform::translation(1.0, 2.0, 3.0);
+
+        assert_eq!(&Transform::translation(1.0, 2.0, 3.0), pattern.transform());
+    }
+
+    #[test]
+    fn a_pattern_with_an_object_transformation() {
+        let mut shape = Sphere::new();
+        *shape.transform_mut() = Transform::scaling(2.0, 2.0, 2.0);
+        let pattern = TestPattern::new();
+        let c = pattern.pattern_at_shape(&shape, &Point3D::new(2.0, 3.0, 4.0));
+
+        assert_eq!(Color::new(1.0, 1.5, 2.0), c);
+    }
+
+    #[test]
+    fn a_pattern_with_both_an_object_and_a_pattern_transformation() {
+        let mut shape = Sphere::new();
+        *shape.transform_mut() = Transform::scaling(2.0, 2.0, 2.0);
+        let mut pattern = TestPattern::new();
+        *pattern.transform_mut() = Transform::translation(0.5, 1.0, 1.5);
+        let c = pattern.pattern_at_shape(&shape, &Point3D::new(2.5, 3.0, 3.5));
+
+        assert_eq!(Color::new(0.75, 0.5, 0.25), c);
+    }
+}
