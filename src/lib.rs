526f4598bd8eb548cb5f5a2d2e77b4755a4b6a62
mtXTJocj <mtXTJocj@yahoo.co.jp>
2021-04-30 11:25:53 +0900

    Cube 追加

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod camera;
 pub mod canvas;
 pub mod checkers_pattern;
 pub mod color;
+pub mod cube;
 pub mod gradient_pattern;
 pub mod intersection;
 pub mod intersection_state;
@@ -22,6 +23,7 @@ pub mod world;
 
 pub type FLOAT = f64;
 const EPSILON: FLOAT = 0.00001;
+const INFINITY: FLOAT = std::f64::INFINITY;
 
 fn approx_eq(a: FLOAT, b: FLOAT) -> bool {
     (a - b).abs() < EPSILON
