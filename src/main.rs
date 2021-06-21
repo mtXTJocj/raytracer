822552f191d99a693ee3a83a97fa5b39d2aa3f2a
mtXTJocj <mtXTJocj@yahoo.co.jp>
2021-06-22 01:24:35 +0900

    main 更新

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use raytracer::{
     camera::Camera, checkers_pattern::CheckersPattern, color::Color,
-    cone::Cone, cylinder::Cylinder, light::Light, node::Node, pattern::Pattern,
-    plane::Plane, point3d::Point3D, shape::Shape, transform::Transform,
-    vector3d::Vector3D, world::World, FLOAT,
+    cone::Cone, cylinder::Cylinder, group::Group, light::Light, node::Node,
+    pattern::Pattern, plane::Plane, point3d::Point3D, shape::Shape,
+    sphere::Sphere, transform::Transform, vector3d::Vector3D, world::World,
+    FLOAT,
 };
 
 use std::{
@@ -11,6 +12,56 @@ use std::{
     fs::File,
     io::{stdout, BufWriter, Write},
 };
+
+fn hexagon_corner() -> Box<Node> {
+    let mut corner = Node::new(Box::new(Sphere::new()));
+    corner.set_transform(
+        &Transform::translation(0.0, 0.0, -1.0)
+            * &Transform::scaling(0.25, 0.25, 0.25),
+    );
+
+    corner
+}
+
+fn hexagon_edge() -> Box<Node> {
+    let mut cyl = Cylinder::new();
+    *cyl.minimum_mut() = 0.0;
+    *cyl.maximum_mut() = 1.0;
+    let mut edge = Node::new(Box::new(cyl));
+    edge.set_transform(
+        &(&(&Transform::translation(0.0, 0.0, -1.0)
+            * &Transform::rotation_y(-std::f64::consts::FRAC_PI_6 as FLOAT))
+            * &Transform::rotation_z(-std::f64::consts::FRAC_PI_2 as FLOAT))
+            * &Transform::scaling(0.25, 1.0, 0.25),
+    );
+
+    edge
+}
+
+fn hexagon_side() -> Box<Node> {
+    let mut side = Node::new(Box::new(Group::new()));
+
+    let corner = hexagon_corner();
+    let edge = hexagon_edge();
+    side.add_child(corner);
+    side.add_child(edge);
+
+    side
+}
+
+fn hexagon() -> Box<Node> {
+    let mut hex = Node::new(Box::new(Group::new()));
+
+    for n in 0..6 {
+        let mut side = hexagon_side();
+        side.set_transform(Transform::rotation_y(
+            n as FLOAT * std::f64::consts::FRAC_PI_3 as FLOAT,
+        ));
+        hex.add_child(side);
+    }
+
+    hex
+}
 
 fn main() {
     let mut writer: Box<dyn Write> = match env::args().nth(1) {
@@ -30,42 +81,12 @@ fn main() {
     *floor.material_mut().pattern_mut() = Some(pattern);
     let mut floor = Node::new(floor);
     floor.set_transform(Transform::translation(0.0, 0.0, 5.0));
-
-    let mut cyl0 = Box::new(Cylinder::new());
-    *cyl0.minimum_mut() = -1.0;
-    *cyl0.maximum_mut() = 1.0;
-    *cyl0.closed_mut() = true;
-    cyl0.material_mut().color = Color::new(0.1, 0.7, 0.7);
-    cyl0.material_mut().reflective = 1.0;
-    cyl0.material_mut().transparency = 0.7;
-    cyl0.material_mut().refractive_index = 0.9;
-    let mut cyl0 = Node::new(cyl0);
-    cyl0.set_transform(
-        &(&(&Transform::translation(-1.0, 1.0, 0.5)
-            * &Transform::rotation_y(std::f32::consts::FRAC_PI_4 as FLOAT))
-            * &Transform::rotation_x(std::f32::consts::FRAC_PI_4 as FLOAT))
-            * &Transform::scaling(0.5, 1.0, 0.5),
-    );
-
-    let mut cone0 = Box::new(Cone::new());
-    *cone0.minimum_mut() = -1.2;
-    *cone0.maximum_mut() = 0.4;
-    *cone0.closed_mut() = true;
-    cone0.material_mut().color = Color::new(0.7, 0.1, 0.7);
-    cone0.material_mut().reflective = 1.0;
-    cone0.material_mut().transparency = 0.7;
-    cone0.material_mut().refractive_index = 0.9;
-    let mut cone0 = Node::new(cone0);
-    cone0.set_transform(
-        &(&(&Transform::translation(1.0, 1.0, 0.5)
-            * &Transform::rotation_y(std::f32::consts::FRAC_PI_4 as FLOAT))
-            * &Transform::rotation_x(std::f32::consts::FRAC_PI_4 as FLOAT))
-            * &Transform::scaling(0.5, 1.0, 0.5),
-    );
-
     world.add_node(floor);
-    world.add_node(cyl0);
-    world.add_node(cone0);
+
+    let mut hex = hexagon();
+    hex.set_transform(Transform::translation(0.0, 0.5, 0.0));
+    world.add_node(hex);
+
     world.add_light(Light::new(
         Point3D::new(-10.0, 10.0, -10.0),
         Color::new(1.0, 1.0, 1.0),
@@ -74,8 +95,8 @@ fn main() {
     let mut camera =
         Camera::new(600, 300, std::f32::consts::FRAC_PI_3 as FLOAT);
     *camera.transform_mut() = Transform::view_transform(
-        &Point3D::new(0.0, 1.5, -5.0),
-        &Point3D::new(0.0, 1.0, 0.0),
+        &Point3D::new(0.0, 3.5, -5.0),
+        &Point3D::new(0.0, 0.0, 0.0),
         &Vector3D::new(0.0, 1.0, 0.0),
     );
 
